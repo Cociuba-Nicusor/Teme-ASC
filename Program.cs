80b4cb187f6c131ea4c717e1feaa3f661c5b4bbe
Cociuba Nicusor <cociubanicusor66@gmail.com>
2020-10-16 13:11:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Moore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;int avansare = 1;int luna = 0;
+            float an = 0;
+            Console.WriteLine("Introduceti viteza puterii de calcul");
+                n =
+                int.Parse(Console.ReadLine());
+            while (n >= avansare) 
+            { avansare = avansare * 2;
+                luna = luna + 18;
+            }
+            Console.WriteLine($"Viteza puterii de calcul {n} va fi pbtinuta in {luna} luni");
+            Console.WriteLine( $"Aproximativ in {an = (float)luna / 12 } ani");
+            Console.ReadKey();
+        }
+    }
+}
